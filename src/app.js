90331472ea4949205279a68a59bb73a7439d1ced
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2024-07-09 02:59:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio] + " ";
+  }
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  console.log(
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when)
+  );
 };
